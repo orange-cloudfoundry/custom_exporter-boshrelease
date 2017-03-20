214fcaa46209eae52954196555e1f54414d353c6
Axel FAUVEL <afauvel.ext@orange.com>
2017-03-20 11:54:26 +0100

    adding script to build and upload release to AF

---
+++ scripts/deliver_release_to_AF.sh
@@ -0,0 +1,24 @@
+#/bin/bash
+
+CUR_DIR=$(pwd)
+CUR_TS=$(date +%s)
+RELEASE_NAME=custom_exporter-${CUR_TS}+alpha.tgz
+
+##release generator for custom_exporter
+rm -Rf tmp_custom_exporter
+git clone https://github.com/orange-cloudfoundry/custom_exporter-boshrelease.git tmp_custom_exporter
+
+cd tmp_custom_exporter
+
+git pull --force --all --verbose --prune && git submodule init && git submodule sync && git submodule foreach git submodule sync && git submodule update --recursive
+
+##add blob
+wget https://storage.googleapis.com/golang/go1.7.linux-amd64.tar.gz
+bosh add-blob go1.7.linux-amd64.tar.gz go1.7.linux-amd64.tar.gz
+
+##creating release
+bosh create-release --tarball=${CUR_DIR}/${RELEASE_NAME} --version ${CUR_TS}+alpha --force
+
+##uploading release
+curl -u ${USER} -T ${CUR_DIR}/${RELEASE_NAME} "https://artifactory.packages.install-os.multis.p.fti.net/cloudfoundry_bosh-release_prometheus_custom_exporter/${RELEASE_NAME}"
+
